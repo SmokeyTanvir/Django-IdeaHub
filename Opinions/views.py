1ca846ae75f26b9adab3fee28598e69c8d238a1f
Tanvir <mazumdersaiful1973@gmail.com>
2023-05-20 13:17:40 +0600

    :pray:

--- Opinions/views.py
+++ Opinions/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpResponseRedirect, JsonResponse
 from .models import Opinion
